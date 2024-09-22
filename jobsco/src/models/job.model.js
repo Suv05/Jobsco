52a798e5f9661e66034701385c27c81b0d22eb88
suvam <suvampravu@gmail.com>
2024-09-23 03:51:38 +0530

    job model funcationality complete

---
+++ jobsco/src/models/job.model.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+const { Schema, model, models } = mongoose;
+
+const JobSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    company: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    jobType: {
+      type: String,
+      enum: ["fulltime", "parttime", "internship", "contract"],
+      required: true,
+    },
+    salaryRange: {
+      min: { type: String },
+      max: { type: String },
+    },
+    experienceLevel: { type: String, required: true },
+    skillsRequired: [{ type: String }],
+    industry: { type: String },
+    educationRequirements: { type: String },
+    applicationDeadline: { type: Date },
+    companyLogo: { type: String },
+    benefits: [{ type: String }],
+    contactEmail: { type: String },
+    userRef: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Job = models.Job || model("Job", JobSchema);
+
+export default Job;
