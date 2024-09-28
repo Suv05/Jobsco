5cf097b8845c6ece14b10d75683d7922acbd2929
suvam <suvampravu@gmail.com>
2024-09-28 23:41:07 +0530

    fetch the posted jobs from backend action

---
+++ jobsco/src/actions/Job-Fetch-Action/index.js
@@ -0,0 +1,59 @@
+import createConnection from "@/db";
+import Job from "@/models/job.model.js";
+import { revalidatePath } from "next/cache";
+
+export async function fetchAllJobs() {
+  try {
+    await createConnection();
+
+    const jobs = await Job.find({});
+
+    if (jobs) {
+      // Use the actual userId in the path
+      revalidatePath(`/`);
+
+      return {
+        status: "success",
+        data: JSON.parse(JSON.stringify(jobs)),
+      };
+    } else {
+      return {
+        status: "error",
+        message: "Failed to fetch jobs",
+      };
+    }
+  } catch (error) {
+    console.log(error);
+    return {
+      status: "error",
+      message: "Failed to fetch jobs",
+    };
+  }
+}
+
+export async function fetchSingleJob(jobId) {
+  try {
+    await createConnection();
+
+    // Fetch single job from the database
+    const job = await Job.findOne({ _id: jobId });
+
+    if (job) {
+      return {
+        status: "success",
+        data: JSON.parse(JSON.stringify(job)),
+      };
+    } else {
+      return {
+        status: "error",
+        message: "Failed to fetch job",
+      };
+    }
+  } catch (error) {
+    console.log(error);
+    return {
+      status: "error",
+      message: "Failed to fetch job",
+    };
+  }
+}       
