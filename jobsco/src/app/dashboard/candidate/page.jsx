602fc6a40bcc26a58f2c0be4160e4cff0188634d
suvam <suvampravu@gmail.com>
2024-10-10 02:11:47 +0530

    finished functionality of premium user

--- jobsco/src/app/dashboard/candidate/page.jsx
+++ jobsco/src/app/dashboard/candidate/page.jsx
@@ -2,6 +2,7 @@ import { currentUser } from "@clerk/nextjs/server";
 import { fetchCandidateInfo } from "@/actions/Fetch-R&C-Info-Action";
 import { totalNumberOfAppliedJobs } from "@/actions/Apply-For-Job-Action";
 import { countSavedJobs } from "@/actions/Saved-Job-Action";
+import { isPrimiumUser } from "@/actions/Add-To-Premium";
 import CDashboard from "@/components/Candidate-Dashboard/CDashboard";
 
 async function CandidateDashboard() {
@@ -9,12 +10,14 @@ async function CandidateDashboard() {
   const { data: candidateInfo } = await fetchCandidateInfo(user?.id);
   const { data: totaljobsApplied } = await totalNumberOfAppliedJobs(user?.id);
   const { data: totalSavedJobs } = await countSavedJobs(user?.id);
+  const { isPro } = await isPrimiumUser(user?.id);
 
   return (
     <CDashboard
       candidateInfo={candidateInfo}
       totalJobs={totaljobsApplied}
       savedJobs={totalSavedJobs}
+      isPro={isPro}
     />
   );
 }
