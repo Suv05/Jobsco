a327c54888d6cf55dd28731a8f35b940ab2137ea
suvam <suvampravu@gmail.com>
2024-10-10 02:11:53 +0530

    finished functionality of premium user

--- jobsco/src/app/pricing/page.jsx
+++ jobsco/src/app/pricing/page.jsx
@@ -1,9 +1,15 @@
 import Pricing from "@/components/Pricing-UI/Pricing";
+import { currentUser } from "@clerk/nextjs/server";
+import { isPrimiumUser } from "@/actions/Add-To-Premium";
 
-export default function page() {
+export default async function page() {
+  const user = await currentUser();
+  const role=user?.unsafeMetadata.role
+
+  const { plan } = await isPrimiumUser(user?.id);
   return (
     <>
-      <Pricing />
+      <Pricing role={role} whichPlan={plan} />
     </>
   );
 }
