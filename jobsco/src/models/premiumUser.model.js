c39f4c28f22c5b8cdf322936f5be0522654cec3d
suvam <suvampravu@gmail.com>
2024-10-09 22:38:41 +0530

    premium user model created

---
+++ jobsco/src/models/premiumUser.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+const { Schema, model, models } = mongoose;
+
+const primiumUserSchema = new Schema({
+  plan: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true,
+  },
+});
+
+const PrimiumUser =
+  models.PrimiumUser || model("PrimiumUser", primiumUserSchema);
+
+export default PrimiumUser;
