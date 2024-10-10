f3becc865497b325829081da6a21b37ffa18d1d2
suvam <suvampravu@gmail.com>
2024-10-10 22:59:15 +0530

    fixing bug

--- jobsco/src/app/[id]/jobs/page.jsx
+++ jobsco/src/app/[id]/jobs/page.jsx
@@ -1,16 +1,29 @@
 import RJobUI from "@/components/Recutor-Listed-JobUI/RJobUI";
-import { fetchRJobs, fetchAppliedCandidates } from "@/actions/Job-Action-By-Recurtor";
-import { auth } from "@clerk/nextjs/server";
+import {
+  fetchRJobs,
+  fetchAppliedCandidates,
+} from "@/actions/Job-Action-By-Recurtor";
+import { auth, currentUser } from "@clerk/nextjs/server";
 
 async function Page() {
-  const { userId } = auth();
+  const { userId, has } = auth();
+  const user = await currentUser();
+
+  //role based authorization
+  const userRole = user?.unsafeMetadata?.role === "recruiter";
+  const canSee = has({ userRole });
+
+  if (!canSee) redirect("/not-found");
 
   const { data: jobs } = await fetchRJobs(userId);
 
   // Fetch applied candidates for each job
   const jobsWithCandidates = await Promise.all(
     jobs.map(async (job) => {
-      const { data: candidates } = await fetchAppliedCandidates(userId, job._id);
+      const { data: candidates } = await fetchAppliedCandidates(
+        userId,
+        job._id
+      );
       return { ...job, candidates };
     })
   );
