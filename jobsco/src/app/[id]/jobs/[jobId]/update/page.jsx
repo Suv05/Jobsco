b64f351aeb50e8dd515d680cedbeeff49f92f358
suvam <suvampravu@gmail.com>
2024-10-10 22:59:12 +0530

    fixing bug

--- jobsco/src/app/[id]/jobs/[jobId]/update/page.jsx
+++ jobsco/src/app/[id]/jobs/[jobId]/update/page.jsx
@@ -1,7 +1,17 @@
 import Updateform from "@/components/update-form/Updateform";
 import { fetchSingleJob } from "@/actions/Job-Action-By-Recurtor";
+import { auth, currentUser } from "@clerk/nextjs/server";
 
 async function page({ params }) {
+  const user = await currentUser();
+  const { has } = auth();
+
+  //role based authorization
+  const userRole = user?.unsafeMetadata?.role === "recruiter";
+  const canSee = has({ userRole });
+
+  if (!canSee) redirect("/not-found");
+
   const jobId = params.jobId;
 
   const { data: fetchData } = await fetchSingleJob(jobId);
