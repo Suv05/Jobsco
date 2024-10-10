b0b398864e77c8e801faf253c21b404f5baab7e4
suvam <suvampravu@gmail.com>
2024-10-10 22:58:56 +0530

    fixing bug

--- jobsco/src/app/jobs/[jobId]/page.jsx
+++ jobsco/src/app/jobs/[jobId]/page.jsx
@@ -1,11 +1,23 @@
 import Jobid from "@/components/Job-Id/Jobid";
 import { fetchSingleJob } from "@/actions/Job-Fetch-Action";
 import { checkIfAlreadyApplied } from "@/actions/Apply-For-Job-Action";
-import { auth } from "@clerk/nextjs/server";
+import { auth, currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 async function page({ params }) {
   const { userId } = auth();
+  const user = await currentUser();
   const { jobId } = params;
+
+ 
+ // Direct role check
+ const isCandidate = user?.unsafeMetadata?.role === "candidate";
+
+ if (!isCandidate) {
+   redirect("/");
+   return null; // Ensure to return null to avoid further processing
+ }
+
   const { data: job } = await fetchSingleJob(jobId);
 
   const { status } = await checkIfAlreadyApplied(jobId, userId);
