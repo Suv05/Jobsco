0e4370560fe4114d700d407e379882201a07801e
suvam <suvampravu@gmail.com>
2024-10-10 22:59:04 +0530

    fixing bug

--- jobsco/src/app/dashboard/recruiter/page.jsx
+++ jobsco/src/app/dashboard/recruiter/page.jsx
@@ -1,14 +1,25 @@
-import { currentUser } from "@clerk/nextjs/server";
+import { currentUser, auth } from "@clerk/nextjs/server";
 import { fetchRecuterInfo } from "@/actions/Fetch-R&C-Info-Action";
 import { fetchRJobs } from "@/actions/Job-Action-By-Recurtor";
 import { fetchNumberOfApplicants } from "@/actions/Apply-For-Job-Action";
+import { isPrimiumUser } from "@/actions/Add-To-Premium";
 
 import RDashboard from "@/components/Recurtor-Dashboard/RDashboard";
+import { redirect } from "next/navigation";
 
 export default async function RecruiterDashboard() {
   const user = await currentUser();
+  const { has } = auth();
+
+  //role based authorization
+  const userRole = user?.unsafeMetadata?.role === "recruiter";
+  const canSee = has({ userRole });
+
+  if (!canSee) redirect("/not-found");
+
   const { data: recruInfo } = await fetchRecuterInfo(user?.id);
   const { data: recruJobInfo } = await fetchRJobs(user?.id);
+  const { isPro: isPro } = await isPrimiumUser(user?.id);
 
   //fetch number of applicants for each job
   const recruiterJobInfoWithApplicants = await Promise.all(
@@ -29,6 +40,7 @@ export default async function RecruiterDashboard() {
       <RDashboard
         recruiterInfo={recruInfo}
         recruiterJobInfo={recruiterJobInfoWithApplicants}
+        isPro={isPro}
       />
     </>
   );
