bbf42bdd34644553c1ce0a696acd338faed696dc
suvam <suvampravu@gmail.com>
2024-10-11 02:29:55 +0530

    UI optimization

--- jobsco/src/app/loading.jsx
+++ jobsco/src/app/loading.jsx
@@ -2,7 +2,7 @@ function Spinner({}) {
   return (
     <>
       <div className="flex items-center justify-center mt-10 mb-10">
-        <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-slate-800"></div>
+        <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-[#0D92F4]"></div>
       </div>
     </>
   );
