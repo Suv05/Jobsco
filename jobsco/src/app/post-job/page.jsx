6109f2a48f5b1a365ad756c952b15488451fd43d
suvam <suvampravu@gmail.com>
2024-10-11 18:26:09 +0530

    feature improvements

--- jobsco/src/app/post-job/page.jsx
+++ jobsco/src/app/post-job/page.jsx
@@ -1,17 +1,15 @@
 import JobPostingForm from "@/components/Post-job/JobPostingForm";
 
-import { auth, currentUser } from "@clerk/nextjs/server";
+import {currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 async function Page({}) {
   const user = await currentUser();
-  const { has } = auth();
 
   //role based authorization
   const userRole = user?.unsafeMetadata?.role === "recruiter";
-  const canSee = has({ userRole });
 
-  if (!canSee) redirect("/not-found");
+  if (!userRole) redirect("/not-found");
   return (
     <>
       <JobPostingForm />
