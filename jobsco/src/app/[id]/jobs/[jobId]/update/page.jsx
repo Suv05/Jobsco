337de11ebb449df520d58c14d661f5f9c7a43013
suvam <suvampravu@gmail.com>
2024-10-11 18:25:59 +0530

    feature improvements

--- jobsco/src/app/[id]/jobs/[jobId]/update/page.jsx
+++ jobsco/src/app/[id]/jobs/[jobId]/update/page.jsx
@@ -1,16 +1,15 @@
 import Updateform from "@/components/update-form/Updateform";
 import { fetchSingleJob } from "@/actions/Job-Action-By-Recurtor";
-import { auth, currentUser } from "@clerk/nextjs/server";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 async function page({ params }) {
   const user = await currentUser();
-  const { has } = auth();
 
   //role based authorization
   const userRole = user?.unsafeMetadata?.role === "recruiter";
-  const canSee = has({ userRole });
 
-  if (!canSee) redirect("/not-found");
+  if (!userRole) redirect("/not-found");
 
   const jobId = params.jobId;
 
