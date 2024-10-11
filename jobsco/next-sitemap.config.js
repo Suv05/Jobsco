89bcaa118c3881208874023aa085c0fa431dc8fb
suvam <suvampravu@gmail.com>
2024-10-11 23:55:45 +0530

    ready for deployment

---
+++ jobsco/next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.yourwebsite.com", // Replace with your website URL
+  generateRobotsTxt: true, // Optionally generate robots.txt file
+};
