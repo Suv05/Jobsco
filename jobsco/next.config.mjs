a775749faf0d875b049bd8af13160e35b259b72f
suvam <suvampravu@gmail.com>
2024-10-11 23:55:50 +0530

    ready for deployment

--- jobsco/next.config.mjs
+++ jobsco/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.4kgroup.com.au",
+        port: "",
+        pathname: "/images/content/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
